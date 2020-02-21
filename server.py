290c5081a138957370fa1e55564b39a583e41b4f
Your Name <you@example.com>
2020-02-20 16:43:23 -0800

    Added html and server routes

--- server.py
+++ server.py
@@ -10,11 +10,47 @@ app = Flask(__name__)
 def get_student():
     """Show information about a student."""
 
-    github = 'jhacks'
+    github = request.args.get('github')
 
     first, last, github = hackbright.get_student_by_github(github)
 
-    return '{github} is the GitHub account for {first} {last}'
+    # return f'{github} is the GitHub account for {first} {last}'
+    return render_template('student_info.html',
+                           first_name=first,
+                           last_name=last,
+                           github_user=github)
+
+@app.route('/search')
+def search():
+    """ Display a form where a user can enter a github id and search for a
+    particular student """
+    return render_template('student_search.html')
+
+
+@app.route('/create_student')
+def create_student():
+
+    return render_template('create_student.html')
+
+@app.route('/add_student', methods=['POST'])
+def add_student():
+
+    fname = request.form.get('fname')
+    lname = request.form.get('lname')
+    github = request.form.get('github')
+
+    hackbright.make_new_student(fname, lname, github)
+
+    link = "/student?github="
+    link += github
+
+    return render_template('confirmation.html',
+                           fname=fname,
+                           lname=lname,
+                           github=github,
+                           link=link)
+
+
 
 
 if __name__ == '__main__':
